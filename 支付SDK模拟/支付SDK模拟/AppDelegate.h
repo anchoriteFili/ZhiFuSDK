6b07318faec873a2b9a45b1d1ff3cd5e0ae5fd00
MAC <mac@MACdeMacBook-Pro.local>
2015-07-30 11:48:40 +0800

    初始化

---
+++ 支付SDK模拟/支付SDK模拟/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  支付SDK模拟
+//
+//  Created by MAC on 15/7/30.
+//  Copyright (c) 2015年 博思创影. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 支付SDK模拟/支付SDK模拟/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  支付SDK模拟
+//
+//  Created by MAC on 15/7/30.
+//  Copyright (c) 2015年 博思创影. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
